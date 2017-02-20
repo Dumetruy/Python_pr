3fd12b42c4a6f64d682d447950395ffc4cc86a47
dvyatkin <dimk67p@gmail.com>
2017-02-20 18:20:37 +0300

    myzip

--- zip.py
+++ zip.py
@@ -3,13 +3,12 @@
 
 def my_zip(*args):
     """func wich do the same as zip()"""
-    return [(tuple([args[j][i] for j in xrange(len(args))])) for i in xrange(min((map(len, args))))]
-
+    return [(tuple([map(list, args)[j][i] for j in xrange(len(args))])) for i in xrange(min((map(len, args))))]
 
 
 L_1 = [1, 2, 3, 4]
 L_2 = ['a', [1, 'b'], 6, 8, 'Kraken']
 L_3 = ['o', 4, 12, 6, 8]
-L_4 = ['args', 456, 'kwargs', 777, 4]
+L_4 = set('Apple')
 
 print my_zip(L_1, L_2, L_3, L_4)

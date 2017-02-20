a1b59050db19bc6bbaefcc89aecbb77852e6adb1
dvyatkin <dimk67p@gmail.com>
2017-02-20 17:42:04 +0300

    new_zip

--- zip.py
+++ zip.py
@@ -1,12 +1,15 @@
 """Zip analog"""
 
 
-def my_zip(one, two):
+def my_zip(*args):
     """func wich do the same as zip()"""
-    return [(one[i], two[i]) for i in xrange(len(min(one, two)))]
+    return [(tuple([args[j][i] for j in xrange(len(args))])) for i in xrange(min((map(len, args))))]
+
 
 
 L_1 = [1, 2, 3, 4]
 L_2 = ['a', [1, 'b'], 6, 8, 'Kraken']
+L_3 = ['o', 4, 12, 6, 8]
+L_4 = ['args', 456, 'kwargs', 777, 4]
 
-my_zip(L_1, L_2)
+print my_zip(L_1, L_2, L_3, L_4)

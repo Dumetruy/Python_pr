7277b19e7f9c4e036a0e7c39de56d0e204cded06
dvyatkin <dimk67p@gmail.com>
2017-02-20 15:12:47 +0300

    my_zip

--- zip.py
+++ zip.py
@@ -1,8 +1,16 @@
 """Zip analog"""
+
+
+def my_zip(one, two):
+    """func wich do the same as zip()"""
+    less = [len(one) if len(one) < len(two) else len(two)]
+    new_l = []
+    for i in xrange(less[0]):
+        new_l.insert(i, (one[i], two[i]))
+    return new_l
+
+
 L_1 = [1, 2, 3, 4]
 L_2 = ['a', [1, 'b'], 6, 8, 'Kraken']
-S = [len(L_1) if len(L_1) < len(L_2) else len(L_2)]
-L_3 = []
-for i in xrange(S[0]):
-    L_3.insert(i, (L_1[i], L_2[i]))
-print L_3
+
+my_zip(L_1, L_2)

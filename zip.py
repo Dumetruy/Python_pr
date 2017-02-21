990e572548124db80f5566e80fe5332e3932b648
dvyatkin <dimk67p@gmail.com>
2017-02-21 14:29:11 +0300

    new_zip

--- zip.py
+++ zip.py
@@ -3,7 +3,7 @@
 
 def my_zip(*args):
     """func wich do the same as zip()"""
-    return [tuple(list(j)[i] for j in args) for i in xrange(min(map(len, args)))]
+    return [tuple(list(arg)[i] for arg in args) for i in xrange(min(map(len, args)))]
 
 
 L_1 = [1, 2, 3, 4]
@@ -11,6 +11,6 @@ L_2 = ['a', [1, 'b'], 6, 8, 'Kraken']
 L_3 = ['o', 4, 12, 6, 8]
 L_4 = set('Apple')
 
-assert zip(L_1, L_2, L_3, L_4), my_zip(L_1, L_2, L_3, L_4)
+assert zip(L_1, L_2, L_3, L_4) == my_zip(L_1, L_2, L_3, L_4)
 
 print my_zip(L_1, L_2, L_3, L_4)

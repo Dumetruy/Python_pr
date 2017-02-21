221a01283237ac2b97e467253f3d629bd26fa9cb
dvyatkin <dimk67p@gmail.com>
2017-02-21 14:32:44 +0300

    mini_func

--- Func.py
+++ Func.py
@@ -1,5 +1,6 @@
 """Few little-func wich do smth"""
 
+
 def square_each(numb):
     """Func wich get square of each element from the list"""
     return [i ** 2 for i in numb]
@@ -13,6 +14,7 @@ def only_even_elem(numb):
 def square_odd_even(numb):
     """Func wich get square of each even element from odd position in the list"""
     return [i**2 for i in numb[1::2] if i % 2 == 0]
+
 
 L_1 = [1, 3, 5, 6, 4, 11, 13, 14, 3, 8]
 

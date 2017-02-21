6f44ce5f33e4cdea792fe05854b2cc2d5a053d51
dvyatkin <dimk67p@gmail.com>
2017-02-21 16:22:53 +0300

    my_xrange()

--- xrange.py
+++ xrange.py
@@ -1,5 +1,6 @@
 """Function wich imitate work of xrange"""
 
+
 def my_own_xrange(start_, stop_=0, step_=1):
     '''Check for stop_ param'''
     if stop_ == 0:
@@ -14,6 +15,8 @@ def my_own_xrange(start_, stop_=0, step_=1):
             yield count
             count += step_
 
+
 L_3 = ['o', 4, 12, 6, 8]
+
 
 print [x for x in my_own_xrange(len(L_3,), 40, 4)]

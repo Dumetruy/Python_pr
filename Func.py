042d626ed3e9dc557e824fd9657eb59ec135a14a
dvyatkin <dimk67p@gmail.com>
2017-02-21 13:08:37 +0300

    mini_func

--- Func.py
+++ Func.py
@@ -5,7 +5,7 @@ def Sec(numb):
     return ([i for i in numb[::2]])
 
 def Sq_c(numb):
-    return [i for i in numb[1::2] if i % 2 == 0]
+    return [i**2 for i in numb[1::2] if i % 2 == 0]
 
 L_1 = [1, 3, 5, 6, 4, 11, 13, 14, 3, 8]
 

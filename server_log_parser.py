3af897c635bed8aa2506275506ad16e9c0e30ac6
dvyatkin <dimk67p@gmail.com>
2017-03-01 11:32:54 +0300

    log_parser

--- server_log_parser.py
+++ server_log_parser.py
@@ -1,41 +1,52 @@
+"""This script read strings from server.log file and print top IP/OS"""
 import sys
 import re
 
 
-def get_data(raw_data):
-    os_name = my_str.group(4).split(';')[0]
-    ip_adr = my_str.group(1)
+def get_data(data_from_re_str):
+    """This func extract data from re.obj"""
+    os_name = data_from_re_str.group(3).split(';')[0].split()[0]
+    ip_adr = data_from_re_str.group(1)
     return ip_adr, os_name
 
 
-def dict_checker(raw_data):
-    if raw_data[0] in ip_dict:
-        ip_dict[raw_data[0]] += 1
+def dict_checker(ip_os_list):
+    """This increment oe add specific IP/OS"""
+    if ip_os_list[0] in IP_DICT:
+        IP_DICT[ip_os_list[0]] += 1
     else:
-        ip_dict[raw_data[0]] = 1
-    if  raw_data[1] in os_dict:
-        os_dict[raw_data[1]] += 1
+        IP_DICT[ip_os_list[0]] = 1
+    if ip_os_list[1] in OS_DICT:
+        OS_DICT[ip_os_list[1]] += 1
     else:
-        os_dict[raw_data[1]] = 1
-        return ip_dict, os_dict
+        OS_DICT[ip_os_list[1]] = 1
+        return IP_DICT, OS_DICT
 
 
 def dic_parser(raw_dict, top_count):
-    for i in xrange(top_count):
-        print sorted(raw_dict.items(), key=lambda (k, v): v, reverse=True)[i]
+    """This func check for len of dict and print sorted one"""
+    if top_count <= len(raw_dict):
+        for i in xrange(top_count):
+            sort_dict = sorted(raw_dict.items(), key=lambda (k, v): v, reverse=True)[i]
+            print '{}: {} - {}'.format(i+1, *sort_dict)
+    else:
+        print 'This dict has only {} records, please enter valid Top number!'.format(len(raw_dict))
 
 
 if __name__ == '__main__':
-    os_dict = {}
-    ip_dict = {}
+    OS_DICT = {}
+    IP_DICT = {}
     try:
         with open(sys.argv[1]) as f:
             for lines in f:
-                my_str = re.match(r'((\d{1,3}\.){3}\d{1,3})(.+)(\((.+)\))', lines)
-                dict_checker(get_data(my_str))
+                my_str = re.match(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).+?(\((.+?)\))', lines)
+                if my_str:
+                    dict_checker(get_data(my_str))
+
     except IOError:
         print "Make sure the file name or path: {}".format(sys.argv[1])
 
-
-    dic_parser(ip_dict, 2)
-    dic_parser(os_dict, 1)
+    print '------IP Top------'
+    dic_parser(IP_DICT, 10)
+    print '------OS Top------'
+    dic_parser(OS_DICT, 5)

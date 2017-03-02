05cf0fffef318d52b3b865b851244c78a1bd7652
dvyatkin <dimk67p@gmail.com>
2017-03-02 15:31:51 +0300

    parser_new

--- parser_stable.py
+++ parser_stable.py
@@ -3,61 +3,68 @@ import sys
 import re
 
 
-def main(script_arg):
+def main():
+    """create 2 dict, check sys.arg[1], init other func"""
     try:
-        file_name = open(script_arg)
-        file_name.close()
-        addr_dict = {}
-        systems_dict = {}
-        parse_file(addr_dict, systems_dict, script_arg)
-        print_result(addr_dict, 10)
-        print_result(systems_dict, 5)
+        script_arg = sys.argv[1]
+    except IndexError:
+        print "Please enter an argument!"
+        return
+
+    ip_dict = {}
+    systems_dict = {}
+    parse_file(ip_dict, systems_dict, script_arg)
+    print_result(ip_dict, 10)
+    print_result(systems_dict, 5)
+
+
+def parse_file(dict_adr, dict_sys, log_name):
+    """try to open file, recieve lines to parse_string finc"""
+    try:
+        with open(log_name) as open_file:
+            for line in open_file:
+                sys_name, addr = parse_string(line)
+                update_dict(dict_adr, addr)
+                update_dict(dict_sys, sys_name)
+            return dict_adr, dict_sys
     except IOError:
-        print "Make sure the file name or path: {}".format(script_arg)
-
-
-def parse_file(dict1, dict2, log_name):
-    try:
-        with open(log_name) as f:
-            for line in f:
-                os, addr = parse_string(line)
-                update_dict(dict1, addr)
-                update_dict(dict2, os)
-            return dict1, dict2
-    except:
-        print "Trouble in this file: {}".format(log_name)
+        print "Can't open {}".format(log_name)
+        exit(1)
 
 
 def parse_string(line_from_log):
-    os_list = ['Metrika', 'Win', 'CrOS', 'Linux', 'FreeBSD', 'Mac OS', 'Slurp', 'Semrush', 'Baiduspider', 'bot', 'coccoc']
+    """takes ip_addr and OS names/bots from lines"""
+    os_list = ['Metrika', 'Win', 'CrOS', 'Linux', 'FreeBSD',
+               'Mac', 'Slurp', 'Semrush', 'Baiduspider', 'bot', 'coccoc']
     ip_name = line_from_log.split()[0]
     sys_str = re.search(r'\((.+?)\)', line_from_log)
     sys_name = 'Validator'
     if sys_str:
         sys_str = sys_str.group(1)
-        for _ in os_list:
-            os_name = re.search(r'(.*?{}.*?)(\s|/)'.format(_), sys_str, re.I)
+        for item in os_list:
+            os_name = re.search(r'(.*?{}.*?)(\s|/)'.format(item), sys_str, re.I)
             if os_name:
-                sys_name =  os_name.group(1).split()[-1]
+                sys_name = os_name.group(1).split()[-1].replace(';', '').replace('Macintosh', 'Mac')
                 break
-    else:
-        pass
     return sys_name, ip_name
 
 
-def update_dict(dict, k):
-    if k in dict:
-        dict[k] += 1
+def update_dict(up_dict, key):
+    """Func that update specific dict"""
+    if key in up_dict:
+        up_dict[key] += 1
     else:
-        dict[k] = 1
-    return dict
+        up_dict[key] = 1
+    return up_dict
 
 
-def print_result(dict, count):
-    sort_dict = sorted(dict.items(), key=lambda (k, v): v, reverse=True)
+def print_result(res_dict, count):
+    """print results from specific dict"""
+    sort_dict = sorted(res_dict.items(), key=lambda (k, v): v, reverse=True)
     print 'Top'.center(23, '-')
-    for n, (key, val) in enumerate(sort_dict[:count], start=1):
-        print '{}: {} - {}'.format(n, key, val)
+    for num, (key, val) in enumerate(sort_dict[:count], start=1):
+        print '{}: {} - {}'.format(num, key, val)
+
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main()

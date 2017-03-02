1c80c803fab2aae25da162e953b086205c4c07f2
Dimka67p@gmail.com <Bred1987az>
2017-03-02 21:37:10 +0300

    new_parses

--- parser_stable.py
+++ parser_stable.py
@@ -14,8 +14,8 @@ def main():
     ip_dict = {}
     systems_dict = {}
     parse_file(ip_dict, systems_dict, script_arg)
-    print_result(ip_dict, 10)
-    print_result(systems_dict, 5)
+    print_result(ip_dict, 10, 'IP')
+    print_result(systems_dict, 5, 'Systems')
 
 
 def parse_file(dict_adr, dict_sys, log_name):
@@ -33,20 +33,28 @@ def parse_file(dict_adr, dict_sys, log_name):
 
 def parse_string(line_from_log):
     """takes ip_addr and OS names/bots from lines"""
-    os_list = ['Metrika', 'Win', 'CrOS', 'Linux', 'FreeBSD',
-               'Mac', 'Slurp', 'Semrush', 'Baiduspider', 'bot', 'coccoc']
+    os_list = ['Win', 'bot', 'Metrika', 'Linux', 'Mac', 'CrOS', 'FreeBSD',
+               'Slurp', 'Semrush', 'Baiduspider', 'coccoc']
     ip_name = line_from_log.split()[0]
     sys_str = re.search(r'\((.+?)\)', line_from_log)
     sys_name = 'Unknown'
     if sys_str:
-        sys_str = sys_str.group(1)
+        sys_str = garbage_cleaner(sys_str.group(1))
         for item in os_list:
             os_name = re.search(r'(.*?{}.*?)(\s|/)'.format(item), sys_str, re.I)
             if os_name:
-                sys_name = os_name.group(1).split()[-1].replace(';', '').replace('Macintosh', 'Mac')
+                sys_name = os_name.group(1).split()[-1]
                 break
     return sys_name, ip_name
 
+
+def garbage_cleaner(garb_str):
+    """clean sting from useless garbage"""
+    for garbage in ['X11;', 'WOW64;', 'MSIE 8.0;', 'MSIE 7.0;', 'compatible;', 'Macintosh;', 'NT', ';', 'x86_64'
+    'MSIE 5.01;', 'MSIE 5.5;', 'MSIE 6.0;', 'MSIE 7.0b;', 'Win64;', ' amd64;', 'bots', '+http://']:
+        garb_str = garb_str.replace(garbage, '')
+        garb_str = garb_str.replace('Win95', 'Windows')
+    return garb_str
 
 def update_dict(up_dict, key):
     """func that update specific dict"""
@@ -57,10 +65,10 @@ def update_dict(up_dict, key):
     return up_dict
 
 
-def print_result(res_dict, depth):
+def print_result(res_dict, depth, name):
     """print results from specific dict with some depth"""
     sort_dict = sorted(res_dict.items(), key=lambda (k, v): v, reverse=True)
-    print 'Top'.center(23, '-')
+    print 'Top {}'.format(name).center(23, '-')
     for num, (key, val) in enumerate(sort_dict[:depth], start=1):
         print '{}: {} - {}'.format(num, key, val)
 

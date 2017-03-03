67749efc516c391e5c60c12ba3ff9513b0cbe422
dvyatkin <dimk67p@gmail.com>
2017-03-03 18:28:17 +0300

    proxy_checker

--- proxy_checker.py
+++ proxy_checker.py
@@ -1,4 +1,9 @@
 import sys
+import re
+import requests
+
+from requests.exceptions import ProxyError
+
 
 def main():
     """check file exist"""
@@ -10,12 +15,40 @@ def main():
     ip_for_check = set()
     valid_ip = set()
     for filename in files:
-        addr = parse_file(filename, ip_for_check)
-        if addr:
-            ip_for_check.add(addr)
+         parse_file(filename, ip_for_check)
+    for i in ip_for_check:
+        print i
+    for addrs in ip_for_check:
+        ip_request(addrs, valid_ip)
+    # print_result(valid_ip)
 
-    ip_request(ip_for_check, valid_ip)
-    print_result(valid_ip)
+def parse_file(name, ip_set):
+    try:
+        with open(name) as ip_list:
+            for line in ip_list:
+                parse_string(line, ip_set)
+    except:
+        print 'Can\'t open!'
+
+def parse_string(line, ip_set):
+    ip_val = re.search(r'((\d{1,3}\.){3}\d{1,3}(\s|\S).*\d{1,5})', line)
+    if ip_val:
+        ip = ''.join(ip_val.group(0).split())
+        ip_set.add(ip)
+
+
+def ip_request(addr, valid_ip_set):
+    req_proxy = {
+        'http': 'http://{}'.format(addr)
+    }
+    try:
+        test_proxy = requests.get('http://www.microsoft.com/ru-ru/', proxies=req_proxy)
+        if test_proxy.ok and 'Microsoft' in test_proxy.text:
+            valid_ip_set.add(addr)
+        else:
+            print 'Nope =('
+    except ProxyError:
+        pass
 
 
 if __name__ == '__main__':

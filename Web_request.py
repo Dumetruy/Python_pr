dd2bcdfae6cd00c9702e7d6d5f045f0305e7f23c
dvyatkin <dimk67p@gmail.com>
2017-03-09 17:31:52 +0300

    beta_beta_01

--- Web_request.py
+++ Web_request.py
@@ -1,9 +1,25 @@
 import requests
 from lxml import html
+import sys
 
-page = requests.get('https://www.google.ru/#newwindow=1&q=Toyota&*')
-tree = html.fromstring(page.content)
 
-buyers = tree.xpath('.//*[@id="rso"]/div[1]/div/div/h3/a')
+def prot1():
+    """check file exist, create sets, init other functions"""
+    #files = sys.argv[1:]
+    #while not files:
+        #print 'Please enter filenames blanked by a whitespace.'
+        #files = raw_input().split()
 
-print 'Link:', buyers
+    page = requests.get('https://www.google.ru/search?q=how+i+met+your+mother')
+    #with open('2.html', 'w') as f:
+        #f.write(page.text.encode('utf8'))
+    tree = html.fromstring(page.text.encode('utf8'))
+    first_link = tree.xpath('.//*[@id="ires"]/ol/div/h3/a/@href')
+    for i in first_link:
+        print i.encode('utf8')
+    #print len(first_link)
+    #print first_link
+
+
+if __name__=='__main__':
+    prot1()

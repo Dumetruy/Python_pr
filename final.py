93e8a93abc183bd57f77b12cf710989c22f32d93
Dimka67p@gmail.com <Bred1987az>
2017-03-27 20:11:07 +0300

    Final

--- final.py
+++ final.py
@@ -1,76 +1,82 @@
 # -*- coding: UTF-8 -*-
-"""Taking flying info from flyniki.com and combine round-trip tickets"""
+"""Flight search on flyniki.com"""
 
 import sys
 import itertools
-from datetime import (datetime, date, timedelta)
+from datetime import datetime, date, timedelta
+
 from lxml import html
 import requests
 
 
 def main():
     """main func"""
-    dest, depart, outbound, return_date = get_usr_data()
-    json_resp = post_resp(depart, dest, outbound, return_date)
+    dest, depart, outbound, return_date = get_user_data()
+    json_resp = get_json_data(depart, dest, outbound, return_date)
     flights_lst, currency = get_fly_list(json_resp, return_date)
     sort_by_cost(flights_lst)
     print_results(depart, dest, currency, flights_lst)
 
 
-def get_usr_data():
+def get_user_data():
     """get user data for flight request"""
     try:
-        depart, dest, there_date = sys.argv[1:4]
-        depart = depart.upper()
-        dest = dest.upper()
+        depart_iata, dest_iata, depart_date = sys.argv[1:4]
         try:
             return_date = sys.argv[4]
-            if return_date >= there_date:
-                validate_date(return_date)
-            else:
-                print 'The return date must be equal to or greater than the date of departure'
-                exit(0)
         except IndexError:
             return_date = ''
-        validate_iata(depart)
-        validate_iata(dest)
-        validate_date(there_date)
-        return dest, depart, there_date, return_date
+        valid_iata_depart, valid_iata_dest = validate_iata(depart_iata, dest_iata)
+        validate_date(depart_date, return_date)
+        return valid_iata_dest, valid_iata_depart, depart_date, return_date
     except ValueError:
         print "Incorrect flight information, data should blanked by a whitespace: AAA AAA YYYY-MM-DD"
         exit(0)
 
 
-def validate_iata(iata_code):
+def validate_iata(*iata_codes):
     """validating IATA code"""
-    if not (iata_code.isalpha() and len(iata_code) == 3):
-        print "Incorrect iata-code format, should be AAA"
-        exit(0)
+    iata_lst = []
+    for code in iata_codes:
+        iata_code = code.upper()
+        if not (iata_code.isalpha() and len(iata_code) == 3):
+            print "Incorrect iata-code format, should be AAA"
+            exit(0)
+        else:
+            iata_lst.append(iata_code)
+    return iata_lst[0], iata_lst[1]
 
 
-def validate_date(date_str):
+def validate_date(depart_date, return_date=''):
     """validating date"""
     try:
-        datetime.strptime(date_str, '%Y-%m-%d')
+        datetime.strptime(depart_date, '%Y-%m-%d')
+        depart_date = datetime.strptime(depart_date, "%Y-%m-%d").date()
+        today_date = date.today()
+        end_date = today_date + timedelta(days=365)
+        if today_date >= depart_date or depart_date > end_date:
+            print 'Incorrect date, should be between tomorrow and 365 days ahead'
+            exit(0)
+        if return_date:
+            return_date = datetime.strptime(return_date, "%Y-%m-%d").date()
+            if return_date <= depart_date:
+                print 'The return date must be equal or greater than the date of departure'
+                exit(0)
+            return depart_date, return_date
+        return depart_date
     except ValueError:
         print 'Incorrect data format, should be YYYY-MM-DD'
         exit(0)
-    user_date = datetime.strptime(date_str, "%Y-%m-%d").date()
-    today_date = date.today()
-    end_date = today_date + timedelta(days=365)
-    if today_date >= user_date or user_date > end_date:
-        print 'Incorrect date, should be between tomorrow and 365 days ahead'
-        exit(0)
 
 
-def post_resp(dep, dest, there, back):
+def get_json_data(dep, dest, depart_date, return_date):
     """getting JSON from post response with flyght details"""
-    oneway = 0 if back else 1
-    back = (back or there)
+    oneway = 0 if return_date else 1
+    return_date = (return_date or depart_date)
     params = {'departure': dep,
               'destination': dest,
-              'outboundDate': there,
-              'returnDate': back,
+              'outboundDate': depart_date,
+              'returnDate': return_date,
               'oneway': oneway,
               'adultCount': '1'}
     headers = {
@@ -81,8 +87,8 @@ def post_resp(dep, dest, there, back):
                  '_ajax[requestParams][destination]': dest,
                  '_ajax[requestParams][returnDeparture]': '',
                  '_ajax[requestParams][returnDestination]': '',
-                 '_ajax[requestParams][outboundDate]': there,
-                 '_ajax[requestParams][returnDate]': back,
+                 '_ajax[requestParams][outboundDate]': depart_date,
+                 '_ajax[requestParams][returnDate]': return_date,
                  '_ajax[requestParams][adultCount]': '1',
                  '_ajax[requestParams][childCount]': '0',
                  '_ajax[requestParams][infantCount]': '0',
@@ -90,34 +96,36 @@ def post_resp(dep, dest, there, back):
                  '_ajax[requestParams][oneway]': oneway}
 
     session = requests.Session()
-    sid = get_sid(session.get('http://www.flyniki.com/ru/booking/flight/vacancy.php',
-                              params=params, headers=headers))
+    session.headers.update(headers)
+    sid = get_sid(session, params)
     return session.post('http://www.flyniki.com/ru/booking/flight/vacancy.php',
                         params={'sid': sid}, data=body_data).json()
 
 
-def get_sid(req_url):
+def get_sid(session, params):
     """get valid sid from get request"""
-    return req_url.url.split('=')[1].encode('utf8')
+    request = session.get('http://www.flyniki.com/ru/booking/flight/vacancy.php',
+                          params=params)
+    return request.url.split('=')[1].encode('utf8')
 
 
 def get_fly_list(json_data, return_date):
     """getting the list of flight infos and the currency"""
     try:
-        tree = html.fromstring(json_data[u'templates'][u'main'])
-        outbound_lst = get_data(get_table(tree, 'outbound'))
+        tree = html.fromstring(json_data['templates']['main'])
+        outbound_flights = get_data(get_flights_trs(tree, 'outbound'))
         curr = get_currency(tree).strip()
         if return_date:
-            return_lst = get_data(get_table(tree, 'return'))
-            return get_product(outbound_lst, return_lst), curr
+            return_flights = get_data(get_flights_trs(tree, 'return'))
+            return get_flights_variants(outbound_flights, return_flights), curr
         else:
-            return outbound_lst, curr
+            return outbound_flights, curr
     except KeyError:
         print "There's now flight with this data, please try with another one!"
         exit(0)
 
 
-def get_table(tree, flight_table):
+def get_flights_trs(tree, flight_table):
     """get specific outbound/return flight table rows"""
     return tree.xpath('.//*[@class="{} block"]//tr[attribute::role]'.format(flight_table))
 
@@ -128,28 +136,28 @@ def get_data(tree_elem):
     for element in tree_elem:
         for elem in element.xpath('td/label/div[1]/span'):
             fly_dict = dict()
-            fly_info_lst = elem.xpath('@title')[0].split(',')
+            fly_info_lst = [item.strip()for item in elem.xpath('@title')[0].split(',')]
             class_cost_lst = fly_info_lst[3].split(':')
-            fly_dict['dep/arv'] = fly_info_lst[1].strip()
-            fly_dict['dur'] = fly_info_lst[2].strip()
-            fly_dict['class'] = class_cost_lst[0].strip()
-            fly_dict['cost'] = float_val(class_cost_lst[1].strip())
+            fly_dict['dep/arv'] = fly_info_lst[1]
+            fly_dict['dur'] = fly_info_lst[2]
+            fly_dict['class'] = class_cost_lst[0]
+            fly_dict['cost'] = get_price(class_cost_lst[1])
             fly_lst.append(fly_dict)
     return fly_lst
 
 
-def float_val(str_val):
+def get_price(str_val):
     """convert cost value from str to float"""
     return float(str_val.replace('.', '').replace(',', '.'))
 
 
-def get_product(dict_there, dict_back):
+def get_flights_variants(outbound_fl_dict, return_fl_dict):
     """combine round-trip tickets by flight class"""
-    product_tuple = itertools.product(dict_there, dict_back)
+    product_tuple = itertools.product(outbound_fl_dict, return_fl_dict)
     variant_list = []
     for variant in product_tuple:
-        count_sum = variant[0]['cost'] + variant[1]['cost']
-        variant_list.append({'flights': variant, 'cost': count_sum})
+        total_sum = variant[0]['cost'] + variant[1]['cost']
+        variant_list.append({'flights': variant, 'cost': total_sum})
     return variant_list
 
 
@@ -167,22 +175,25 @@ def sort_by_cost(unsorted_item):
     unsorted_item.sort(key=lambda k: k['cost'])
 
 
-def print_results(depart, dur, curr, sorted_flights):
+def print_results(iata_depart, iata_dest, curr, sorted_flights):
     """printing results"""
-    head_template = 'From: {} To: {} Currency:{}'
-    fish_str = 'Depart/Arrive:{dep/arv} Duration:{dur} Cost:{cost} Class:{class}'
-    flight = 'Flight #{}'
-    for idx, item in enumerate(sorted_flights):
-        if len(item) != 4:
-            print head_template.format(depart, dur, curr).center(180, ' ')
-            print flight.format(idx).center(180, ' ')
-            out_fly = item['flights'][0]
+    print 'From: {} To: {}'.format(iata_depart, iata_dest)
+    banners_str = 'Depart/Arrive   Duration        Class          Cost'
+    fish_str = '{dep/arv} | {dur} | {class} | {cost}'
+    flight = '#{}'
+    for i, item in enumerate(sorted_flights):
+        if 'flights' in item:
+            there_fly = item['flights'][0]
             ret_fly = item['flights'][1]
-            print fish_str.format(**out_fly), '<<< THERE == BACK >>>', fish_str.format(**ret_fly)
+            print flight.format(i)
+            print banners_str
+            print fish_str.format(**there_fly), curr
+            print fish_str.format(**ret_fly), curr
+            print 'Total: {}'.format(item['cost']), curr, '\n'
         else:
-            print head_template.format(depart, dur, curr).center(75, ' ')
-            print flight.format(idx).center(75, ' ')
-            print fish_str.format(**item)
+            print flight.format(i)
+            print banners_str
+            print fish_str.format(**item), curr, '\n'
 
 
 if __name__ == "__main__":
